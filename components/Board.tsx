20b28069b96ef98941c1c03c36b253043cb352e1
skamath99 <sankethkamath99@gmail.com>
2024-11-04 19:41:33 -0500

    Creating Board Component

---
+++ components/Board.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+const Board = () => {
+    return <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>I am a board</Text>
+    </View>
+}
+
+export default Board;
